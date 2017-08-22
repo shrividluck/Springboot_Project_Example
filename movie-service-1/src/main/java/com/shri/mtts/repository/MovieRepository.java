650605d331692cb547232a310c1c910a045c6ecf
shrividluck <shrividluck@gmail.com>
2017-08-21 21:38:38 -0700

    Final submission version

--- movie-service-1/src/main/java/com/shri/mtts/repository/MovieRepository.java
+++ movie-service-1/src/main/java/com/shri/mtts/repository/MovieRepository.java
@@ -13,21 +13,11 @@ import com.shri.mtts.entity.impl.MovieImpl;
 
 
 public interface MovieRepository extends CrudRepository<MovieImpl, Long> {
-	
-	//Iterable<MovieImpl> findByPincodeContainingAndMoviesPlayingContaining(long pincode, String movieName); //???
-   
+  
 	Iterable<MovieImpl> findByGenreContaining(String genre);
     
-    // ??? to get list of theaters playing the movie based on movieName.
-	//Iterable<MovieImpl> findByTheaterNameContaining(String theaterName); 
-
-	// return castCrew , synopsis and ratings from DB.
 	Iterable<MovieImpl> findByMovieNameContaining(String movieName); 
 	
-	// return ExactMovie.
-     MovieImpl findByMovieName(String movieName); 
-	
-	// return movieImpl based on id param
-	//Iterable<MovieImpl> finfBymovieId(Long id);
+    MovieImpl findByMovieName(String movieName); 
 	
 }
